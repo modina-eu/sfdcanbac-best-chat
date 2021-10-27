9e5496aa8d80737d9559dd065168613c17e9c01b
Glitch (hello-webpage) <none>
2021-10-27 21:44:57 +0000

    📰🎐 Checkpoint ./style.css:9838890/559 ./script.js:9838890/10580 ./index.html:9838890/77

--- script.js
+++ script.js
@@ -21,9 +21,144 @@ class HydraApp extends Torus.StyledComponent {
 class CodeApp extends Torus.StyledComponent {
   init() {
     this.el = document.createElement("TEXTAREA");
+    
+    
+// https://github.com/ojack/hydra/blob/3dcbf85c22b9f30c45b29ac63066e4bbb00cf225/hydra-server/app/src/editor.js
+const flashCode = (l0, l1) => {
+  if (l0 === undefined) l0 = cm.firstLine();
+  if (l1 === undefined) l1 = cm.lastLine() + 1;
+  let count = 0;
+  for(let l = l0; l < l1; l++) {
+    const start = { line: l, ch: 0 };
+    const end = { line: l + 1, ch: 0 };
+    setTimeout(() => {
+      const marker = cm.markText(start, end, { className: "styled-background" });
+      setTimeout(() => marker.clear(), 300);
+    }, count * 500 / (Math.max(1, l1 - l0)));
+    count++;
+  }
+};
+
+const getLine = () => {
+  var c = cm.getCursor();
+  var s = cm.getLine(c.line);
+  flashCode(c.line, c.line + 1);
+  return s;
+};
+
+const getCurrentBlock = () => {
+  // thanks to graham wakefield + gibber
+  var editor = cm;
+  var pos = editor.getCursor();
+  var startline = pos.line;
+  var endline = pos.line;
+  while (startline > 0 && cm.getLine(startline) !== "") {
+    startline--;
+  }
+  while (endline < editor.lineCount() && cm.getLine(endline) !== "") {
+    endline++;
+  }
+  var pos1 = {
+    line: startline,
+    ch: 0
+  };
+  var pos2 = {
+    line: endline,
+    ch: 0
+  };
+  var str = editor.getRange(pos1, pos2);
+
+  flashCode(startline, endline);
+
+  return str;
+};
+
+const editorConsoleText = document.getElementById("editor-console-text");
+const evalCode = (c) => {
+  try {
+    let result = eval(c);
+    if (result === undefined) result = "";
+    editorConsoleText.innerText = result;
+    editorConsoleText.className = "normal";
+    localStorage.setItem("hydracode", cm.getValue());
+  } catch (e) {
+    console.log(e);
+    editorConsoleText.innerText = e;
+    editorConsoleText.className = "error";
+  }
+}
+
+toggleCode = () {
+  if (container.style.visibility == "hidden") {
+    container.style.visibility = "inherit";
+  } else {
+    container.style.visibility = "hidden";
+  }
+}
+
+const commands = {
+  evalAll: () => {
+    const code = cm.getValue();
+    flashCode();
+    evalCode(code);
+  },
+  toggleEditor: () => {
+    toggleCode();
+  },
+  evalLine: () => {
+    const code = getLine();
+    evalCode(code);
+  },
+  toggleComment: () => {
+    cm.toggleComment();
+  },
+  evalBlock: () => {
+    const code = getCurrentBlock();
+    evalCode(code);
+  }
+};
+
+const keyMap = {
+  evalAll: { key: "ctrl+shift+enter" },
+  toggleEditor: { key: "ctrl+shift+h" },
+  toggleComment: { key: "ctrl+/" },
+  evalLine: { key: "shift+enter,ctrl+enter" },
+  evalBlock: { key: "alt+enter" }
+};
+
+// enable in textarea
+hotkeys.filter = function(event) {
+  return true;
+};
+const commandNames = Object.keys(keyMap);
+for (const commandName of commandNames) {
+  const hk = keyMap[commandName];
+  if (typeof commands[commandName] === "function") {
+    hotkeys(hk.key, function(e, hotkeyHandler) {
+      e.preventDefault();
+      commands[commandName]();
+    });
+  }
+}
+
+  }
+  styles() {
+    return css`
+.editor-console {
+  font-family: monospace;
+  font-variant-ligatures: no-common-ligatures;
+  font-size: 14pt;
+  color: #000000;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+`
   }
   render() {
-    super.render();
+    let r = super.render();
     if (this.cm == undefined) {
       this.cm = CodeMirror.fromTextArea(this.el, {
         theme: "paraiso-dark",
@@ -37,6 +172,8 @@ class CodeApp extends Torus.StyledComponent {
       );
       setTimeout(() => this.cm.refresh(), 0);
     }
+    this.cm.refresh()
+    return r;
   }
   compose() {
     return jdom`
