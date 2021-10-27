0acb854fe14e969aea57fa957222f12c71571dbd
Glitch (hello-webpage) <none>
2021-10-27 22:17:04 +0000

    🌻📉 Checkpoint ./script.js:9838890/2186

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ class HydraApp extends Torus.StyledComponent {
       detectAudio: false,
       enableStreamCapture: false
     });
-    osc().out();
     window.addEventListener('resize',
-      () => this.hydra.resize(window.innerWidth, window.innerHeight), true);
+      () => {
+      this.hydra.setResolution(window.innerWidth, window.innerHeight);
+      // this.canvas.width = window.innerWidth;
+      // this.canvas.height = window.innerHeight;
+    }, true);
   }
   compose() {
     return this.canvas;
@@ -213,73 +216,3 @@ class App extends Torus.StyledComponent {
 
 const app = new App();
 document.querySelector("div#app").appendChild(app.node);
-
-// // https://github.com/ojack/hydra/blob/3dcbf85c22b9f30c45b29ac63066e4bbb00cf225/hydra-server/app/src/editor.js
-// const flashCode = function(start, end) {
-//   if (!start) start = { line: cm.firstLine(), ch: 0 };
-//   if (!end) end = { line: cm.lastLine() + 1, ch: 0 };
-//   var marker = cm.markText(start, end, { className: "styled-background" });
-//   setTimeout(() => marker.clear(), 300);
-// };
-
-// const getLine = function() {
-//   var c = cm.getCursor();
-//   var s = cm.getLine(c.line);
-//   flashCode({ line: c.line, ch: 0 }, { line: c.line + 1, ch: 0 });
-//   return s;
-// };
-
-// const getCurrentBlock = function () { // thanks to graham wakefield + gibber
-//   var editor = cm
-//   var pos = editor.getCursor()
-//   var startline = pos.line
-//   var endline = pos.line
-//   while (startline > 0 && cm.getLine(startline) !== '') {
-//     startline--
-//   }
-//   while (endline < editor.lineCount() && cm.getLine(endline) !== '') {
-//     endline++
-//   }
-//   var pos1 = {
-//     line: startline,
-//     ch: 0
-//   }
-//   var pos2 = {
-//     line: endline,
-//     ch: 0
-//   }
-//   var str = editor.getRange(pos1, pos2)
-
-//   flashCode(pos1, pos2)
-
-//   return str
-// }
-
-// {
-//   // init
-//   const code = cm.getValue();
-//   hydra.eval(code);
-// }
-
-// window.onkeydown = e => {
-//   if (cm.hasFocus()) {
-//     if (e.keyCode === 13) {
-//       e.preventDefault();
-//       if (e.ctrlKey === true && e.shiftKey === true) {
-//         // ctrl - shift - enter: evalAll
-//         const code = cm.getValue();
-//         flashCode();
-//         hydra.eval(code);
-//       } else if (e.ctrlKey === true && e.shiftKey === false) {
-//         // ctrl - enter: evalLine
-//         const code = getLine();
-//         hydra.eval(code);
-//       } else if (e.altKey === true) {
-//         // alt - enter: evalBlock
-//         const code = getCurrentBlock();
-//    console.log(code)
-//         hydra.eval(code);
-//       }
-//     }
-//   }
-// };
