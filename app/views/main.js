0e2cbe531b98dc9a1431653edfde2777e56761f3
Glitch (dances) <none>
2023-08-17 07:38:34 +0000

    👆🏜 Checkpoint ./package.json:9838890/7195 ./app/index.js:9838890/663 ./app/views/main.js:9838890/4754

---
+++ app/views/main.js
@@ -0,0 +1,14 @@
+import html from "choo/html";
+import { css } from "@emotion/css";
+
+const mainCss = css`
+`;
+
+// export module
+export default function(state, emit) {
+  return html`
+    <div class=${ mainCss }>
+      hallo
+    </div>
+  `;
+}
