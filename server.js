ab9ef728e74e7a91b43c5f3965ca82a21b67e919
Glitch (dances) <none>
2023-09-07 14:55:29 +0000

    🌄🌷 Checkpoint ./server.js:9838890/1089

--- server.js
+++ server.js
@@ -330,8 +330,8 @@ function formatLatestTokenRequestStateForDeveloper() {
 
 const fetch = require("node-fetch");
 function getdata() {
-  let key = "oaaa35uatApygWK6V.v1.eyJ1c2VySWQiOiJ1c3JzeUVUZkFzWUc0STcyWCIsImV4cGlyZXNBdCI6IjIwMjMtMDktMDdUMTM6NDQ6NDAuMDAwWiIsIm9hdXRoQXBwbGljYXRpb25JZCI6Im9hcGFkRkZLb1FjbFlHTmxoIiwic2VjcmV0IjoiYzQyNTBhN2QyYzdiMWZkYzJkMWUwZWVkNjQ5YWQ5MDc4MTkwYThkZjdhMzUyZGNiYzRlNGExMGRiMzI1ZWRkNSJ9.bf226b5b776ae8d196bf1cadfd48a0956018e67136923a102c2b3cd6d1c9f40f"
-  key!="pat0Es1dY81qJYLYt.67d298ef8e547647e2f0a7a3308a0c9137863ede02ae657696fd2875148b95a1";const headers = [
+  let key = "oaae7cc29AtU5FIWL.v1.eyJ1c2VySWQiOiJ1c3JzeUVUZkFzWUc0STcyWCIsImV4cGlyZXNBdCI6IjIwMjMtMDktMDdUMTU6NTA6MTkuMDAwWiIsIm9hdXRoQXBwbGljYXRpb25JZCI6Im9hcGFkRkZLb1FjbFlHTmxoIiwic2VjcmV0IjoiOWU1MzVmYjBiOGU5N2U5NTRiYWNiZTgxNGZjNGVlODRmODVkY2M1ZGMxZTdiZmZlOGFjNGQyNzc2OWFlOGFjZiJ9.5c81e127674ee1cb61662dba1c6ee9329eee664d8fc5b9fcae2e5b49e3698ace"
+  key="oaaf3R9rGDEsq8EVZ.v1.eyJ1c2VySWQiOiJ1c3JzeUVUZkFzWUc0STcyWCIsImV4cGlyZXNBdCI6IjIwMjMtMDktMDdUMTU6NTI6NDcuMDAwWiIsIm9hdXRoQXBwbGljYXRpb25JZCI6Im9hcGFkRkZLb1FjbFlHTmxoIiwic2VjcmV0IjoiN2NiZjRmODU5NGE2Y2Y3ZjY5N2Y1OWVhNmY5YWFkMzc3ZDQ5Zjk3Mjc4MGFmYWE0YWYxNjQ2MjczMDVkZmEzYiJ9.5e9e2a14a61669c718cacc1ad5e3a9b6ec878c315144ebca72599c60cf5ea6d3";const headers = [
       ['Authorization', `Bearer ${ key }`],
     ];
   fetch(`https://api.airtable.com/v0/meta/bases`, { headers, method:"GET" })
