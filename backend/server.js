51f6ae65219fc20fada9e186ba7b66499082fb00
Glitch (dances) <none>
2023-09-29 08:31:02 +0000

    🔨🐣 Checkpoint ./backend/server.js:9838890/75 ./frontend/vite.config.js:9838890/296 ./package.json:9838890/2

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.BACKEND_PORT;
+const port = !!process.env.BACKEND_PORT ? process.env.BACKEND_PORT : 40000;
 
 app.get('/api/getrandomhello', async function(req, res, next) {
   res.json({ data: ["hello!", "hola!", "salut", "hallo"][Math.floor(Math.random()*4)] });
