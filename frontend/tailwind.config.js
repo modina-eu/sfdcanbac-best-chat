b4d9bd68c556bf8249e895f2f287f5e6bc4a64ac
Glitch (dances) <none>
2023-10-20 13:01:43 +0000

    🐪🏏 Checkpoint ./backend/server.js:9838890/1508 ./package.json:9838890/1861 ./tailwind.config.js:9838890/200 ./postcss.config.js:9838890/82 ./frontend/pages/main.js:9838890/39

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  content: [
+    "./index.html",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [],
+}
+
