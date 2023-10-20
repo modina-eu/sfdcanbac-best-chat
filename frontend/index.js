6d2447d5d547a343f9ec6522d6d00743e98a1899
Glitch (naoto-status) <none>
2023-10-20 17:52:37 +0000

    ✍️💾 Checkpoint ./frontend/index2.js:9838890/176 ./frontend/index.html:9838890/640 ./frontend/index.js:9838890/382 ./backend/server.js:9838890/17

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,8 @@
+// import htmx from "htmx.org";
+// import 'htmx.org/dist/ext/preload.js';
 // import choo
 import choo from "choo";
 import html from "choo/html";
-import htmx from "htmx.org";
 
 // initialize choo
 const app = choo({ hash: true, href: false });
@@ -17,7 +18,27 @@ function notFound() {
 }
 
 import mainView from "./pages/main.js";
-app.route("/", mainView);
+app.route("/", function () {
+  return html`
+    <div>
+      you are the
+        <span hx-post="/api/counter" hx-trigger="load">
+          loading
+        </span>th visitor
+      <div id="parent-div"></div>
+      <button hx-post="/api/clicked"
+        hx-trigger="click"
+        hx-target="#parent-div"
+        hx-swap="outerHTML"
+      >
+        Click Me!
+      </button>
+
+      <div hx-post="/api/clicked" hx-trigger="click">Click Me</div>
+      <script src="https://unpkg.com/htmx.org@1.9.6" />
+    </div>
+  `;
+});
 
 app.use((state, emitter) => {
   state.data = { data: "no data yet"}
@@ -26,3 +47,5 @@ app.use((state, emitter) => {
 // start app
 app.mount("#choomount");
 
+app.emitter.on("DOMContentLoaded", () => {
+})
