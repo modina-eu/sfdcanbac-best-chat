89f067703c93c2123db60ba4608f5cce189d71e7
Glitch (naoto-status) <none>
2023-10-20 13:31:00 +0000

    Rewound to commit c72918c22c3986b94d5ab673e98dd57bdd73a9db + 0 edits

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,9 @@ import dotenv from "dotenv";
 import express from 'express';
 const app = express();
 
-// import { AceBase } from "acebase";
-// const options = { logLevel: 'warn', storage: { path: '.' } }; // optional settings
-// const db = await new AceBase('mydb', options);  // Creates or opens a database with name "mydb"
-
 app.use(express.json());
 
-const port = !!process.env.BACKEND_PORT ? process.env.BACKEND_PORT : 40000;
+const port = process.env.BACKEND_PORT;
 
 app.get('/api/getrandomhello', async function(req, res, next) {
   res.json({ data: ["hello!", "hola!", "salut", "hallo"][Math.floor(Math.random()*4)] });
@@ -18,44 +14,3 @@ app.get('/api/getrandomhello', async function(req, res, next) {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// app.post('/api/upload', upload.single('imgfile'), async function(req, res, next) {
-//   try {
-//     superagent
-//       .post("https://api.imgbb.com/1/upload")
-//       .field('key', process.env.IMGBB_KEY)
-//       .attach("image", req.file.path)
-//       .field("name", req.file.originalname)
-//       .end(async (err, res_) => {
-//         if (err) {
-//           console.log(err);
-//           res.send("error");
-//         } else {
-//           unlinkAsync(req.file.path);
-//           console.log('Media uploaded!');
-//           db.ref('images')
-//             .push({
-//               image: res_.body.data.image,
-//               thumb: res_.body.data.thumb,
-//               medium: res_.body.data.medium,
-//               date: +new Date,
-//             })
-//             .then(userRef => {
-//             });
-//           res.send("ok");
-//         }
-//     })
-//   } catch (error) {
-//     console.error("Error:", error);
-//     res.send("error");
-//   }
-// })
-
-// app.get('/api/geturls', async function(req, res, next) {
-//   const snapshot = await db.ref('images').get();
-//   let data = snapshot.val();
-//   const keys = Object.keys(data);
-//   data = keys.map(key => {return {...data[key], key}}).sort((a, b) => b.date - a.date);
-
-//   res.json(data.slice(0, 10));
-// })
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,50 @@
+import dotenv from "dotenv";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  const commonConfig = {
-    plugins: [],
+export default defineConfig(async ({ command, mode }) => {
+  return {
     root: '.',
+    plugins: [
+    ],
     build: {
-      minify: false,
-      outDir: "build",
-      commonjsOptions: {
+      cssCodeSplit: false,
+      outDir: "./build",
+      rollupOptions: {
+        context: 'globalThis',
+        input: {
+          app: './index.html',
+        },
+        output: {
+          // 2️⃣
+          entryFileNames: 'assets/js/[name].js',
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`
+        },
+      },
+    },
+    optimizeDeps: {
+      esbuildOptions: {
       }
+    },
+    define: {
+      global: "globalThis",
     },
     server: {
       host: "0.0.0.0",
-      port: 3000,
+      port: process.env.FRONTEND_PORT,
       strictPort: true,
       hmr: {
-        clientPort: 443 // Run the websocket server on the SSL port
+        clientPort: 443
+      },
+      proxy: {
+        '/api': {
+          target: `http://localhost:${ process.env.BACKEND_PORT }`,
+          changeOrigin: true,
+          secure: false,
+        }
       }
-    }
+    },
   };
-  if (command === 'serve') {
-    return {
-      ...commonConfig,
-      define: {
-        // fix for hydra-synth
-        global: "window"
-      },  
-    }
-  } else {
-    // command === 'build'
-    return {
-      ...commonConfig
-    }
-  }
-})
+});
