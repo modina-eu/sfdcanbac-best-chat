7cf12728ec43861a81420c1e21fdeff629d7edf6
Glitch (naoto-status) <none>
2023-12-19 23:55:21 +0000

    🕶😻 Checkpoint ./backend/content.js:9838890/1189 ./frontend/index.html:9838890/374

--- backend/content.js
+++ backend/content.js
@@ -1,22 +1,12 @@
 import express from 'express';
 const router = express.Router()
 
-import AirtableLoader from "./libs/airtable-loader.js";
-
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
 
 TimeAgo.addDefaultLocale(en)
 
 const timeAgo = new TimeAgo('en-US')
-
-const airtableLoader = new AirtableLoader(
-  process.env.AIRTABLE_KEY,
-  process.env.AIRTABLE_ID,
-  "Table 1",
-  "Grid view"
-);
-airtableLoader.poll();
 
 router.get('/api/content', async function(req, res) {
   res.set({
@@ -31,34 +21,12 @@ router.get('/api/content', async function(req, res) {
   let count = 0;
   
   req.on('close', () => {
-    airtableLoader.eventEmitter.removeListener("airtable updated", writeData);
   });
 
   function writeData() {    
-    res.write("data: <div> " + airtableLoader.elements.map(e => `
-      <div>
-        <div>
-          <span class="text-gray-600">${ timeAgo.format(new Date(e.created)) }</span> <span>${ e.notes ? e.notes : "" }</span>
-        </div>
-        ${ e.images.map(e => `<img class="w-full max-w-xs" src=${ e } />`).join("") }
-      </div>
-    `).join("").replace(/\n/g, "") + "</div>\n\n");
+    res.write("data: <div>a</div>\n\n");
   }
   writeData();
-  airtableLoader.eventEmitter.on("airtable updated", writeData);
 });
-
-// router.post('/api/content', async function(req, res, next) {
-//   res.send(airtableLoader.elements.map(e => `
-//     <div>
-//       <div>
-//         <span class="text-gray-600">${ timeAgo.format(new Date(e.created)) }</span> <span>${ e.notes ? e.notes : "" }</span>
-//       </div>
-//       ${ e.images.map(e => `<img class="w-full max-w-xs" src=${ e } />`).join("") }
-//     </div>
-//   `).join(""));
-// });
-
-router.use('/api/images', express.static('images'))
 
 export default router;
