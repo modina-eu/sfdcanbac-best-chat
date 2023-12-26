71eb8723cde8759505d99becc487e810bdfad377
Glitch (naoto-status) <none>
2023-12-26 02:44:00 +0000

    🌮💺 Checkpoint ./backend/db.js:9838890/10 ./.gitignore:9838890/57

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 import { AceBase } from "acebase";
 const options = { logLevel: 'warn', storage: { path: '.' } }; // optional settings
-const db = await new AceBase('.mydb', options);  // Creates or opens a database with name ".mydb"
+const db = await new AceBase('../.data/mydb', options);  // Creates or opens a database with name ".mydb"
 
 export default db;
